8fc8a15a9998629b5dbf93960d88c47378b456b2
vladiik124 <vladimirkotliar8@gmail.com>
2025-03-04 19:55:21 +0200

    created repo

---
+++ main.py
@@ -0,0 +1,17 @@
+import discord
+import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in ooooooseiseisei as {client.user}")
+
+client.run(os.getenv('TOKEN'))
